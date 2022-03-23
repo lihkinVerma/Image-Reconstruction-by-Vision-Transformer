4ac5008ba918796673aec44ed398e0a57ef74250
lihkinVerma <lih.verma@gmail.com>
2022-03-23 16:41:19 -0400

    adding changes to vision tranformer by adding variable for RoPE embedding

--- transformers_for_imaging/mymodels/vision_transformer.py
+++ transformers_for_imaging/mymodels/vision_transformer.py
@@ -155,6 +155,23 @@ def init_weights(m):
             nn.init.constant_(m.bias, 0)
             nn.init.constant_(m.weight, 1.0)
 
+def exists(val):
+    return val is not None
+
+def rotate_every_two(x):
+    x = rearrange(x, '... (d j) -> ... d j', j = 2)
+    x1, x2 = x.unbind(dim = -1)
+    x = torch.stack((-x2, x1), dim = -1)
+    return rearrange(x, '... d j -> ... (d j)')
+
+def apply_rotary_pos_emb(q, k, sinu_pos):
+    sinu_pos = rearrange(sinu_pos, '() n (j d) -> n j d', j = 2)
+    sin, cos = sinu_pos.unbind(dim = -2)
+
+    sin, cos = map(lambda t: repeat(t, 'b n -> b (n j)', j = 2), (sin, cos))
+    q, k = map(lambda t: (t * cos) + (rotate_every_two(t) * sin), (q, k))
+    return q, k
+
 class MHSA(nn.Module):
     def __init__(self, dim, num_patches, heads=8, dim_head=64, dropout=0., is_LSA=False):
         super().__init__()
@@ -180,10 +197,12 @@ class MHSA(nn.Module):
         else:
             self.mask = None
 
-    def forward(self, x):
+    def forward(self, x, pos_emb = None):
         b, n, _, h = *x.shape, self.heads
         qkv = self.to_qkv(x).chunk(3, dim=-1)
         q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=h), qkv)
+        if exists(pos_emb):
+            q, k = apply_rotary_pos_emb(q, k, pos_emb)
 
         if self.mask is None:
             dots = einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
@@ -200,130 +219,6 @@ class MHSA(nn.Module):
         out = rearrange(out, 'b h n d -> b n (h d)')
         return self.to_out(out)
 
-"""
-class MHSA(nn.Module):
-    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0., grid_size=None, num_patches=None, is_LSA=False):
-        super().__init__()
-        self.num_heads = num_heads
-        head_dim = dim // num_heads
-        self.scale = qk_scale or head_dim ** -0.5
-
-        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
-        self.attn_drop = nn.Dropout(attn_drop)
-        self.proj = nn.Linear(dim, dim)
-        self.proj_drop = nn.Dropout(proj_drop)
-        self.apply(self._init_weights)
-        self.current_grid_size = grid_size
-        self.num_patches = num_patches
-
-        if is_LSA:
-            self.scale1 = nn.Parameter(self.scale * torch.ones(self.num_heads))
-            self.mask = torch.eye(self.num_patches + 1, self.num_patches + 1)
-            self.mask = torch.nonzero((self.mask == 1), as_tuple=False)
-        else:
-            self.mask = None
-
-    def _init_weights(self, m):
-        if isinstance(m, nn.Linear):
-            trunc_normal_(m.weight, std=.02)
-            if isinstance(m, nn.Linear) and m.bias is not None:
-                nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.LayerNorm):
-            nn.init.constant_(m.bias, 0)
-            nn.init.constant_(m.weight, 1.0)
-
-    def get_attention_map(self, x, return_map = False):
-        self.get_rel_indices()
-        B, N, C = x.shape
-        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
-        q, k, v = qkv[0], qkv[1], qkv[2]
-        attn_map = (q @ k.transpose(-2, -1)) * self.scale
-        attn_map = attn_map.softmax(dim=-1).mean(0) # average over batch
-        distances = self.rel_indices.squeeze()[:,:,-1]**.5
-        dist = torch.einsum('nm,hnm->h', (distances, attn_map))
-        dist /= distances.size(0)
-        if return_map:
-            return dist, attn_map
-        else:
-            return dist
-
-    def get_rel_indices(self, ):
-        H, W = self.current_grid_size
-        N = H*W
-        rel_indices = torch.zeros(1, N, N, 3)
-        indx = torch.arange(W).view(1,-1) - torch.arange(W).view(-1, 1)
-        indx = indx.repeat(H, H)
-        indy = torch.arange(H).view(1,-1) - torch.arange(H).view(-1, 1)
-        indy = indy.repeat_interleave(W, dim=0).repeat_interleave(W, dim=1)
-        indd = indx**2 + indy**2
-        rel_indices[:,:,:,2] = indd.unsqueeze(0)
-        rel_indices[:,:,:,1] = indy.unsqueeze(0)
-        rel_indices[:,:,:,0] = indx.unsqueeze(0)
-        device = self.qkv.weight.device
-        self.rel_indices = rel_indices.to(device)
-
-    def forward(self, x):
-        b, n, _, h = x.shape[0], x.shape[1], x.shape[2], self.num_heads
-        qkv = self.qkv(x).chunk(3, dim=-1)
-        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=h), qkv)
-        print(q.shape,k.shape,v.shape)
-
-        if self.mask is None:
-            dots = einsum('b h i d, b h j d -> b h i j', q, k) * self.scale1
-
-        else:
-            scale = self.scale1
-            dots = torch.mul(einsum('b h i d, b h j d -> b h i j', q, k),
-                             scale.unsqueeze(0).unsqueeze(-1).unsqueeze(-1).expand((b, h, 1, 1)))
-            dots[:, :, self.mask[:, 0], self.mask[:, 1]] = -987654321
-
-        attn = dots.softmax(dim=-1)
-        print("-------", attn.shape)
-        out = einsum('b h i j, b h j d -> b h i d', attn, v)
-
-        out = rearrange(out, 'b h n d -> b n (h d)')
-
-        x = self.proj(out)
-        x = self.proj_drop(x)
-
-        return x
-
-    def forward(self, x):
-        B, N, C = x.shape
-        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
-        q, k, v = qkv[0], qkv[1], qkv[2]
-        print("+++++++++", q.shape, k.shape, v.shape)
-
-        # attn1 = (q @ k.transpose(-2, -1)) * self.scale
-        # print("========", attn1.shape)
-        if self.mask is None:
-            dots = einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
-
-        else:
-            scale = self.scale1
-            dots = torch.mul(einsum('b h i d, b h j d -> b h i j', q, k),
-                             scale.unsqueeze(0).unsqueeze(-1).unsqueeze(-1).expand((B, self.num_heads, 1, 1)))
-            dots[:, :, self.mask[:, 0], self.mask[:, 1]] = -987654321
-            print(dots.shape)
-
-        # attn1 = attn1.softmax(dim=-1)
-        # print("-----", attn1.shape)
-        # attn1 = self.attn_drop(attn1)
-        # print(attn1.shape)
-        attn = dots.softmax(dim=-1)
-        print("===========", attn.shape)
-        attn = self.attn_drop(attn)
-        print(attn.shape)
-
-        # x = (attn @ v).transpose(1, 2).reshape(B, N, C)
-        out = einsum('b h i j, b h j d -> b h i d', attn, v)
-
-        x = rearrange(out, 'b h n d -> b n (h d)')
-        x = self.proj(x)
-        x = self.proj_drop(x)
-
-        return x
-        """
 
 class Block(nn.Module):
     def __init__(self, dim, num_heads,  mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
@@ -341,9 +236,9 @@ class Block(nn.Module):
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
 
-    def forward(self, x, grid_size):
+    def forward(self, x, grid_size, pos_emb = None):
         self.attn.current_grid_size = grid_size
-        x = x + self.drop_path(self.attn(self.norm1(x)))
+        x = x + self.drop_path(self.attn(self.norm1(x), pos_emb = pos_emb))
         x = x + self.drop_path(self.mlp(self.norm2(x)))
 
         return x
@@ -370,16 +265,33 @@ class PatchEmbed(nn.Module):
             nn.init.constant_(m.bias, 0)
             nn.init.constant_(m.weight, 1.0)
 
+class FixedPositionalEmbedding(nn.Module):
+    def __init__(self, dim, max_seq_len):
+        super().__init__()
+        inv_freq = 1. / (10000 ** (torch.arange(0, dim, 2).float() / dim))
+        position = torch.arange(0, max_seq_len, dtype=torch.float)
+        sinusoid_inp = torch.einsum("i,j->ij", position, inv_freq)
+        emb = torch.cat((sinusoid_inp.sin(), sinusoid_inp.cos()), dim=-1)
+        self.register_buffer('emb', emb)
+
+    def forward(self, x):
+        #print(x.shape[1])
+        #print(self.emb.shape)
+        return self.emb[None, :x.shape[1], :].to(x)
+
+
 class VisionTransformer(nn.Module):
     """ Vision Transformer
     """
     def __init__(self, avrg_img_size=320, patch_size=10, in_chans=1, embed_dim=64, depth=8,
                  num_heads=9, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop_rate=0., attn_drop_rate=0.,
                  drop_path_rate=0., norm_layer=nn.LayerNorm, global_pool=None,
-                 gpsa_interval=[-1, -1], locality_strength=1., use_pos_embed=True, is_LSA=False, is_SPT=False):
+                 gpsa_interval=[-1, -1], locality_strength=1., use_pos_embed=True,
+                 is_LSA=False, is_SPT=False, rotary_position_emb = False):
 
         super().__init__()
         self.depth = depth
+        self.embed_dim = embed_dim
         embed_dim *= num_heads
         self.num_features = embed_dim  # num_features for consistency with other models
         self.locality_strength = locality_strength
@@ -435,6 +347,10 @@ class VisionTransformer(nn.Module):
         # head
         self.feature_info = [dict(num_chs=embed_dim, reduction=0, module='head')]
         self.head = nn.Linear(self.num_features, in_chans*self.patch_size[0]*self.patch_size[1])
+        max_seq_len = 1025
+        self.layer_pos_emb = None
+        if rotary_position_emb:
+            self.layer_pos_emb = FixedPositionalEmbedding(self.embed_dim, max_seq_len)
 
     def seq2img(self, x, img_size):
         """
@@ -481,9 +397,13 @@ class VisionTransformer(nn.Module):
 
         x = x.flatten(2).transpose(1, 2)
         x = self.pos_drop(x)
+        if self.layer_pos_emb is not None:
+            layer_pos_emb = self.layer_pos_emb(x)
+        else:
+            layer_pos_emb = None
 
         for u, blk in enumerate(self.blocks):
-            x = blk(x, (H, W))
+            x = blk(x, (H, W), pos_emb = layer_pos_emb)
 
         x = self.norm(x)
 
